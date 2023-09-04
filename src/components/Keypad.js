34b0eb2a1157acee0b976b0fcf28a2608fb8efba
owen <owen.chosen@gmail.com>
2023-09-04 13:23:52 +0100

    Done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,7 +2,11 @@
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={
+                (event) => console.log ("Entering password...")
+            }/>
+        </div>
     )
 }
 
